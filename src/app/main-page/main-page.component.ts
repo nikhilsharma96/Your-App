7b1d958d38b3ba3861b5ee164be85d2b67d536e7
Nikhil Sharma <atnikhil96@gmail.com>
2021-01-17 15:42:01 +0530

    done

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -9,11 +9,12 @@ import { ApiCallsService } from '../services/api-calls.service';
 export class MainPageComponent implements OnInit {
 
   constructor(private service:ApiCallsService) { }
-  icon=["&#128521;","&#128519;"]
+  icon=["&#128521;","&#128519;","&#128070","&#128071","&#128072","&#128073"]
   insult;
   quote;
   showModal=false;
   result:string;
+  emoji;
   ngOnInit(): void {
     this.insult=this.service.getInsult();
     this.quote=this.service.getQuote();
@@ -21,9 +22,11 @@ export class MainPageComponent implements OnInit {
   showSomething(value:string){
     if(value=='quote'){
       this.quote.subscribe((data)=>{this.result=data["quote"].body});
+      this.emoji="😊 🙏"
     }
     else{
       this.insult.subscribe((data)=>{this.result=data["insult"]});
+      this.emoji="🤣 🖕";
     }
   }
 }
